afda42f36f900961420cfc1a2193f8efe5579dbb
Zubair Ahmed <STr1k3R768@gmail.com>
2025-05-24 20:26:21 +0500

    Task 01 Complete

---
+++ js/task_01.js
@@ -0,0 +1,23 @@
+// assignment_01 | Welcome Message
+let userName = prompt('What is your name?')
+alert(`Welcome, ${userName}.`)
+
+// assignment_02 | Age Message
+let userAge = prompt('How old are you.')
+alert(`${userName} is ${userAge} years old.`)
+
+// assignment_03 | Favorite Color
+let favoriteColor = prompt('What is your favorite color?')
+alert(`Wow! ${favoriteColor} is a nice color!`)
+
+// assignment_04 | Simple Math Sentence
+let num1 = +prompt('Enter any number of your choice')
+let num2 = +prompt('Enter any number of your choice')
+
+alert(`The result of ${num1} + ${num2} is ${num1 + num2}.`)
+
+// assignment_05 | About Me Summary
+let userCountry = prompt("In which country do you live?")
+let userHobby = prompt("What is your hobby?")
+
+alert(`Hi, I'm ${userName} from ${userCountry}, and I love ${userHobby}!`)
