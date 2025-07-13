cdd2cb95d77241a84a17b888e1c9ffbfc400dc8a
Zubair Ahmed <STr1k3R768@gmail.com>
2025-07-13 21:23:12 +0500

    Assignment 08 updated Confirmation Button Color Changed

--- assets/js-assignments/assignment_08/assignment_08.js
+++ assets/js-assignments/assignment_08/assignment_08.js
@@ -8,13 +8,15 @@ function toDoApp() {
             title: "Input Required!",
             text: "You need to type something first.",
             icon: "info",
-            confirmButtonText: "OK"
+            confirmButtonText: "OK",
+            confirmButtonColor: "#3085d6"
         });
         return;
     }
     toDos.push(toDo);
     toDoRender();
-    console.log(toDos)
+    document.getElementById("todoText").value = "";
+
 }
 
 function toDoRender() {
@@ -47,6 +49,9 @@ function toDoRender() {
         );
 
         const editBtn = document.createElement("button");
+        editBtn.classList.add(
+            "cursor-pointer"
+        )
         editBtn.innerHTML = `
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ff8080" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-pen-icon lucide-square-pen"><path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/></svg>
         `;
@@ -56,6 +61,9 @@ function toDoRender() {
         };
 
         const deleteBtn = document.createElement("button");
+        deleteBtn.classList.add(
+            "cursor-pointer"
+        )
         deleteBtn.innerHTML = `
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ff8080" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
         `;
@@ -89,6 +97,8 @@ function toDoEdit(index) {
         showCancelButton: true,
         confirmButtonText: "Save",
         cancelButtonText: "Cancel",
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
         inputValidator: (value) => {
             if (!value.trim()) {
                 return "To-do cannot be empty!";
