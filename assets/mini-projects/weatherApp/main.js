65981bbebc0665713afd55a614c53e25a36de4d2
Zubair Ahmed <STr1k3R768@gmail.com>
2025-10-05 02:36:08 +0500

    Weather App html page title updated City search input and button added WeatherApi added in js file remove old code form WeatherApp/main.js file

--- assets/mini-projects/weatherApp/main.js
+++ assets/mini-projects/weatherApp/main.js
@@ -1,161 +1,117 @@
-const toDos = [];
-const todoList = document.getElementById("todoList");
+// * Weather Data Fetch
+// api = http://api.weatherapi.com/v1/current.json?key=539c403662ee408eb60151203250410&q=London&aqi=no
+// const staticApi = "http://api.weatherapi.com/v1/current.json?key=539c403662ee408eb60151203250410&q=London&aqi=no"
 
-function toDoApp() {
-    const toDo = document.getElementById("todoText").value;
-    if (toDo.length === 0) {
-        Swal.fire({
-            title: "Input Required!",
-            text: "You need to type something first.",
-            icon: "info",
-            confirmButtonText: "OK",
-            confirmButtonColor: "#3085d6"
-        });
-        return;
-    }
-    toDos.push(toDo);
-    toDoRender();
-    document.getElementById("todoText").value = "";
 
+let searchedLocations = []
+const weatherLocations = [
+    { city: "Quetta", country: "PK", temperature: "29C" },
+    { city: "Karachi", country: "PK", temperature: "30C" },
+    { city: "Islamabad", country: "PK", temperature: "25C" },
+]
+
+const weatherSearchButton = document.getElementById("weather-search-button")
+const weatherSearchLocation = document.getElementById("weather-search")
+const weatherRender = document.getElementById("weather-render")
+// const weatherClearButton = document.getElementById("weather-clear-button")
+
+// * currentDate
+const getCurrentDate = () => {
+    const today = new Date()
+    const day = today.getDate()
+    const month = today.toLocaleString('default', { month: 'short' }) // "Oct", "Nov", etc.
+    const year = today.getFullYear()
+    return `${day} ${month} ${year}`
+}
+const getDay = () => {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const date = new Date()
+    const day = days[date.getDay()]
+    return day
 }
 
-function toDoRender() {
-    todoList.textContent = "";
 
-    for (let i = 0; i < toDos.length; i++) {
-        const li = document.createElement("li");
-        li.classList.add(
-            "flex",
-            "justify-between",
-            "items-center",
-            "my-2",
-            "hover:bg-gray-300",
-            "p-2",
-            "rounded-3xl"
-        );
-
-        const span = document.createElement("span");
-        span.classList.add(
-            "overflow-scroll"
-        )
-        span.textContent = toDos[i];
-
-        const buttonContainer = document.createElement("span");
-        buttonContainer.classList.add(
-            "flex",
-            "justify-between",
-            "items-center",
-            "gap-2"
-        );
-
-        const editBtn = document.createElement("button");
-        editBtn.classList.add(
-            "cursor-pointer"
-        )
-        editBtn.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ff8080" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-pen-icon lucide-square-pen"><path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/></svg>
-        `;
-
-        editBtn.onclick = function () {
-            toDoEdit(i);
-        };
-
-        const deleteBtn = document.createElement("button");
-        deleteBtn.classList.add(
-            "cursor-pointer"
-        )
-        deleteBtn.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ff8080" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
-        `;
-        deleteBtn.onclick = function () {
-            toDoDelete(i);
-        };
-
-        buttonContainer.appendChild(editBtn);
-        buttonContainer.appendChild(deleteBtn);
-
-        li.appendChild(span);
-        li.appendChild(buttonContainer);
-
-        todoList.appendChild(li);
+weatherSearchButton.addEventListener("click", () => {
+    const cityLocation = weatherSearchLocation.value
+    if(!cityLocation) {
+        console.log("Please type city name")
+        return
     }
-}
 
-// function toDoEdit(index) {
-//   const newValue = prompt("Edit your todo:", toDos[index]);
-//   if (newValue !== null && newValue.trim() !== "") {
-//     toDos[index] = newValue.trim();
-//     toDoRender();
-//   }
-// }
+    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=539c403662ee408eb60151203250410&q=${cityLocation}&aqi=no`
+    // const apiUrl = `http://api.weatherapi.com/v1/current.json?key={my_key_here}&q=${cityLocation}&aqi=no`
 
-function toDoEdit(index) {
-    Swal.fire({
-        title: "Edit your to-do",
-        input: "text",
-        inputValue: toDos[index],
-        showCancelButton: true,
-        confirmButtonText: "Save",
-        cancelButtonText: "Cancel",
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        inputValidator: (value) => {
-            if (!value.trim()) {
-                return "To-do cannot be empty!";
+    fetch(apiUrl)
+        .then((res) => { return res.json() })
+        .then((data) => {
+
+            // weatherObjects
+            const location = data.location;
+            const current = data.current;
+
+            if (cityLocation === location.name) {
+                if (!searchedLocations.some(item => item.location.name === location.name)) {
+                    searchedLocations.push(data)
+                }
             }
-        }
-    }).then((result) => {
-        if (result.isConfirmed) {
-            toDos[index] = result.value.trim();
-            toDoRender();
-        }
-    });
-}
 
+            weatherRender.innerHTML = ''
 
-function toDoDelete(index) {
-    Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!",
-    }).then((result) => {
-        if (result.isConfirmed) {
-            toDos.splice(index, 1);
-            toDoRender();
-            Swal.fire({
-                title: "Deleted!",
-                text: "To-Do deleted.",
-                icon: "success",
-            });
-        }
-    });
-}
+            searchedLocations.forEach(item => {
+                const location = item.location;
+                const current = item.current;
 
-function toDoDeleteAll() {
-    Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete all!",
-    }).then((result) => {
-        if (result.isConfirmed) {
-            toDos.length = 0;
+                weatherRender.innerHTML += `
+            <div class="flex flex-col justify-between h-80 w-full bg-gradient-to-r from-green-300 to-cyan-400 hover:from-green-400 hover:to-cyan-500 transition p-4 rounded-2xl mt-4">
+                <div>
+                    <div>
+                        <span class="font-bold">${getDay()}</span>
+                    </div>
+                    <div>
+                        <span>${getCurrentDate()}</span>
+                    </div>
+                    <div class="flex items-center mt-2">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin-icon lucide-map-pin"><path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"/><circle cx="12" cy="10" r="3"/>
+                            </svg>
+                        </span>
+                        <div class="flex flex-col pl-2">
+                            <span>
+                                ${location.name}
+                            </span>
+                            <span>
+                                ${location.region}, ${location.country}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="self-center">
+                    <div class="flex flex-col justify-center items-center">
+                        <div>
+                            <img class="w-30" src="${current.condition.icon}" alt="">
+                        </div>
+                        <div class="text-2xl font-bold">${current.temp_c} °C</div>
+                        <div class="font-semibold">
+                            <span>${current.condition.text}</span>
+                        </div>
+                        <div class="font-light">
+                            <span>Feels like ${current.feelslike_c} °C</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+        `
+            })
+        }).catch((error) => {
+            console.log(error)
+        })
+})
 
-            toDoRender();
-
-            Swal.fire({
-                title: "Deleted All!",
-                text: "All to-dos deleted.",
-                icon: "success",
-            });
-        }
-    });
-}
-
+// weatherLocations.forEach(item => {
+//     if(cityLocation === item.city) {
+//         if(!searchedLocations.some(weatherLocations => weatherLocations.city === item.city)) {
+//             searchedLocations.push(item)
+//         }
+//     }
+// })
